decab081b50b2b9b19ee312ab1def98f58c3aa5d
sachinites <sachinites@gmail.com>
2021-05-23 23:15:44 -0700

    Conn Mgmt Stage set

--- ConnMgmt/conn_mgmt.c
+++ ConnMgmt/conn_mgmt.c
@@ -19,58 +19,140 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <memory.h>
+#include <assert.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <memory.h>
+#include <errno.h>
+#include <netdb.h> 
+#include <unistd.h>
 #include "conn_mgmt.h"
 
-void
+static void
 conn_mgmt_init_conn_thread(
 	conn_mgmt_conn_thread_t *conn_thread) {
 
-
+    pthread_mutex_init(&conn_thread->mutex, 0);
+    pthread_cond_init(&conn_thread->cv, 0);
+    conn_thread->thread_fn = 0;
+    conn_thread->thread_fn_arg = 0;
 }
 
 conn_mgmt_conn_state_t *
 conn_mgmt_create_new_connection(
-    char *dest_ip,
-    uint32_t port_no,
-    conn_mgmt_proto_t proto,
-    uint16_t ka_interval,
-    conn_mgmt_app_notif_fn_ptr app_notif_cb) {
+    conn_mgmt_conn_key_t *conn_key) {
 
-	int i;
+    conn_mgmt_conn_state_t *conn;
 
-	conn_mgmt_conn_state_t *conn = calloc(1, sizeof(conn_mgmt_conn_state_t));	
-	strncpy(conn->dest, dest_ip, 16);
-	conn->proto = proto;
+	conn = calloc(1, sizeof(conn_mgmt_conn_state_t));	
 
-	switch(conn->proto) {
-
-		case COMM_MGMT_UDP:
-			conn->pon.dst_udp_port = port_no;
-			break;
-		case COMM_MGMT_TCP:
-			conn->pon.dst_tcp_port = port_no;
-			break;
-		default:
-			;
-	}
-	conn->keep_alive_interval = ka_interval;
+    memcpy(&conn->conn_key, conn_key, sizeof(conn_mgmt_conn_key_t));
+    conn->mastership_state = COMM_MGMT_MASTER;
+	conn->keep_alive_interval = CONN_MGMT_DEFAULT_KA_INTERVAL;
 	conn_mgmt_init_conn_thread(&conn->conn_thread);
 	conn->ka_recvd = 0;
 	conn->ka_sent = 0;
 	conn->down_count = 0;
-
-	for ( i = 0; i < CONN_MGMT_MAX_CLIENTS_SUPPORTED; i++) {
-		if (!conn->app_notif_cb[i]) {
-			conn->app_notif_cb[i] = app_notif_cb;
-			break;
-		}
-	}
-
-	if ( i == CONN_MGMT_MAX_CLIENTS_SUPPORTED) {
-		free(conn);
-		return NULL;
-	}
-
-	init_glthread(&conn->glue);
 	return conn;
 }
+
+void
+conn_mgmt_set_conn_ka_interval(
+        conn_mgmt_conn_state_t *conn,
+        uint32_t ka_interval) {
+    
+    conn->keep_alive_interval = ka_interval;
+}
+
+#define MAX_PACKET_BUFFER_SIZE 256
+static unsigned char recv_buffer[MAX_PACKET_BUFFER_SIZE];
+
+
+static void
+pkt_receive( conn_mgmt_conn_state_t *conn,
+			 unsigned char *pkt,
+			 uint32_t pkt_size) {
+
+}
+
+
+static void*
+conn_mgmt_pkt_recv(void *arg) {
+
+	uint32_t bytes_recvd;
+    
+    int addr_len = sizeof(struct sockaddr);
+    
+    int udp_sock_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP );
+    
+    conn_mgmt_conn_state_t *conn = (conn_mgmt_conn_state_t *)arg;
+    
+    struct sockaddr_in sender_addr;
+    sender_addr.sin_family      = AF_INET;
+    sender_addr.sin_port        = conn->conn_key.src_port_no;
+    sender_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    if (bind(udp_sock_fd, (struct sockaddr *)&sender_addr, sizeof(struct sockaddr)) == -1) {
+        printf("Error : socket bind failed\n");
+        return 0;
+    }
+    
+    while(1) {
+    
+    	memset(recv_buffer, 0, MAX_PACKET_BUFFER_SIZE);
+        bytes_recvd = recvfrom(udp_sock_fd, (char *)recv_buffer, 
+                        MAX_PACKET_BUFFER_SIZE, 0, (struct sockaddr *)&sender_addr, &addr_len);
+                
+        pkt_receive(conn, recv_buffer, bytes_recvd);
+    }    
+    
+    return 0;
+}
+
+static void
+conn_mgmt_start_pkt_recvr_thread(conn_mgmt_conn_state_t *conn) {
+
+	pthread_attr_t attr;
+    pthread_t recv_pkt_thread;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    pthread_create(&recv_pkt_thread, &attr, 
+                    conn_mgmt_pkt_recv, (void *)conn);
+}
+
+
+static void *
+conn_mgmt_send_ka_pkt(void *arg) {
+
+	
+}
+
+static void
+conn_mgmt_start_ka_sending_thread(
+        conn_mgmt_conn_state_t *conn) {
+
+	pthread_attr_t attr;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+
+    pthread_create(&conn->ka_thread_handle, &attr, 
+                    conn_mgmt_send_ka_pkt, (void *)conn);
+}
+
+
+void
+conn_mgmt_start_connection(
+        conn_mgmt_conn_state_t *conn) {
+
+    assert(conn->keep_alive_interval);
+
+    conn_mgmt_start_pkt_recvr_thread(conn);
+    conn_mgmt_start_ka_sending_thread(conn);
+}
+
+
+
+
--- ConnMgmt/conn_mgmt.h
+++ ConnMgmt/conn_mgmt.h
@@ -31,87 +31,65 @@ typedef enum {
 
 typedef enum {
 
-	COMM_MGMT_UDP,
-	COMM_MGMT_TCP
+	COMM_MGMT_PROTO_UDP,
+	COMM_MGMT_PROTO_TCP
 } conn_mgmt_proto_t;
 
-typedef void *(*conn_mgmt_app_notif_fn_ptr)(
-				conn_mgmt_conn_status_t conn_code, 
-				char *machine_ip,
-				uint32_t port_no,
-				void *msg,
-				uint32_t msg_size);
+typedef enum {
+
+    COMM_MGMT_MASTER,
+    COMM_MGMT_BACKUP
+} conn_mgmt_mastership_state;
 
 #define CONN_MGMT_MAX_CLIENTS_SUPPORTED	8
+#define CONN_MGMT_DEFAULT_KA_INTERVAL   5
 
 typedef struct comm_mgmt_conn_thread_ {
 
-	pthread_t conn_thread_handle;
-	pthread_mutex_t mutx;
+	pthread_t ka_thread_handle;
+	pthread_mutex_t mutex;
 	pthread_cond_t cv;
 	void *(*thread_fn)(void *);
 	void *thread_fn_arg;
-	bool should_pause;
 } conn_mgmt_conn_thread_t;
+
+typedef struct conn_mgmt_conn_key_ {
+
+    unsigned char dest_ip[16];
+    unsigned char src_ip[16];
+    uint32_t src_port_no;
+    uint32_t dst_port_no;
+    conn_mgmt_proto_t proto;
+} conn_mgmt_conn_key_t;
+
+typedef void *(*conn_mgmt_app_notif_fn_ptr)(
+				conn_mgmt_conn_status_t conn_code, 
+                conn_mgmt_conn_key_t *conn_key,
+				void *msg,
+				uint32_t msg_size);
 
 typedef struct conn_mgmt_conn_state_{
 
-	char dest[16];
-	conn_mgmt_proto_t proto;
-	union {
-		uint32_t dst_udp_port;
-		uint32_t dst_tcp_port;
-	}pon;
+    conn_mgmt_conn_key_t conn_key;
+    conn_mgmt_mastership_state mastership_state;
+    conn_mgmt_conn_status_t conn_status;
 	uint16_t keep_alive_interval;
 	conn_mgmt_conn_thread_t conn_thread;
 	uint32_t ka_recvd;
 	uint32_t ka_sent;
 	uint32_t down_count;
 	conn_mgmt_app_notif_fn_ptr app_notif_cb[CONN_MGMT_MAX_CLIENTS_SUPPORTED];
-	glthread_t glue;
 } conn_mgmt_conn_state_t;
-
-GLTHREAD_TO_STRUCT(glue_to_conn_state_t, conn_mgmt_conn_state_t, glue);
-
-typedef struct conn_mgmt_conn_db_ {
-
-	glthread_t head;
-	uint32_t no_of_connections;
-} conn_mgmt_conn_db_t;
 
 conn_mgmt_conn_state_t *
 conn_mgmt_create_new_connection(
-	char *dest_ip,
-	uint32_t port_no,
-	conn_mgmt_proto_t proto,
-	uint16_t ka_interval,
-	conn_mgmt_app_notif_fn_ptr app_notif_cb);
+    conn_mgmt_conn_key_t *conn_key);
 
-conn_mgmt_conn_state_t *
-conn_mgmt_lookup_connection(
-	char *dest_ip,
-	uint32_t port_no);
+void
+conn_mgmt_set_conn_ka_interval(conn_mgmt_conn_state_t *conn,
+                               uint32_t ka_interval);
 
-bool
-conn_mgmt_update_connection(
-	char *dest_ip,
-	uint32_t port_no,
-	conn_mgmt_conn_state_t *new_conn_state);
-
-bool
-conn_mgmt_stop_connection(
-	char *dest_ip,
-	uint32_t port_no);
-
-bool
-conn_mgmt_destroy_connection(
-	char *dest_ip,
-	uint32_t port_no);
-
-bool
-conn_mgmt_resume_connection(
-	char *dest_ip,
-	uint32_t port_no);
-
+void
+conn_mgmt_start_connection(conn_mgmt_conn_state_t *conn);
 
 #endif /* __CONN_MGMT__  */
